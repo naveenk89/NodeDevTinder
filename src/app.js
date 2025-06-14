496c497c883877e3088d7ad52a21802bf70fe3d0
sammyreddy5 <samathareddy520@gmail.com>
2025-06-14 15:13:55 -0500

    Exploring

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 
 const app = express();
-app.use("/",(req,res)=>{
-    res.send("namaste from the dashboard...");
-})
-app.use("/test",(req,res)=>{
-    res.send("test from the server...");
-})
 app.use("/hello",(req,res)=>{
     res.send("Hello from the server...");
 })
+// app.use("/",(req,res)=>{
+//     res.send("namaste from the dashboard...");
+// })
+app.use("/test",(req,res)=>{
+    res.send("test from the server...");
+})
+app.get("/user",(req,res)=>{
+    res.send({firstName:"Naveen",lastName:"K"});
+})
+app.post("/user",(req,res)=>{
+    res.send("Data sucessfully saved to the dashboard");
+})
 
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully")
+})
 app.listen(3000,()=> {
     console.log("server is listening on port numberr 3000")
 });
